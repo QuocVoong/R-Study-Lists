ba3385842323f4f425ca410e53e7b87ec50180d1
robinlan <robin_lan2003@yahoo.com.tw>
2014-12-12 13:12:59 +0800

    papers

---
+++ DIY labs/Speech Recognition/13.4 非特定人语音识别算法——HMM - joey_su的专栏 - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,35 @@
+﻿var ad_type = 'ms1111';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+//document.writeln(ad_sc);
+
+ ad_type = 'ms1113_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms1116_3';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/*
+ ad_type = 'js1107_4';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms846_5';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+*/
---
+++ DIY labs/Speech Recognition/13.4 非特定人语音识别算法——HMM - joey_su的专栏 - 博客频道 - CSDN.NET_files/ad-ms1116_3.js
@@ -0,0 +1,53 @@
+﻿//bbs 
+var cloudad_type3 = 'ms1116_3';
+var cloudad_urls3 = [
+'http://ad.csdn.net/adsrc/shuguang-dec-download-ziyuanye-xiafang-banner-728-90-30k.swf'
+];
+var cloudad_clks3 = [
+''
+];
+
+var can_swf3 = (function () {
+    if (document.all) swf = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+    else if (navigator.plugins) swf = navigator.plugins["Shockwave Flash"];
+    return !!swf;
+})();
+
+function cloudad_show3() {
+    var rd = Math.random();
+    var ad_url, log_url;
+    if (rd < 0.96 && can_swf3) {
+        ad_url = cloudad_urls3[0];
+
+        log_url = 'http://ad.csdn.net/log.ashx';
+        log_url += '?t=view&adtype=' + cloudad_type3 + '&adurl=' + encodeURIComponent(ad_url);
+        cloudad_doRequest3(log_url, true);
+    }
+    if (rd < 0) {
+        ad_url = cloudad_clks3[0];
+
+        log_url = 'http://ad.csdn.net/log.ashx';
+        log_url += '?t=click&adtype=' + cloudad_type3 + '&adurl=' + encodeURIComponent(ad_url);
+        cloudad_doRequest3(log_url, true);
+    }
+}
+
+function cloudad_doRequest3(url, useFrm) {
+    var e = document.createElement(useFrm ? "iframe" : "img");
+
+    e.style.width = "1px";
+    e.style.height = "1px";
+    e.style.position = "absolute";
+    e.style.visibility = "hidden";
+
+    if (url.indexOf('?') > 0) url += '&r_m=';
+    else url += '?r_m=';
+    url += new Date().getMilliseconds();
+    e.src = url;
+
+    document.body.appendChild(e);
+}
+
+setTimeout(function () {
+    cloudad_show3();
+}, 1000);
